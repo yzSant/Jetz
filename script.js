0ba6bda19f45bc1ba3a69b18b187f3d847d04deb
Luana <luana.developer7@gmail.com>
2024-11-28 08:40:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const sr = ScrollReveal({reset: true});
+
+sr.reveal("#img-home", {
+    origin: "left", 
+    distance: "50px", 
+    duration: 2000, 
+    delay: 10,
+    reset: true,
+  });
+
+sr.reveal("#text-home", {
+  origin: "right", 
+  distance: "50px", 
+  duration: 2000, 
+  delay: 10,
+  reset: true,
+});
+
+
+
+
+  
+  
+
+  
+
+  
